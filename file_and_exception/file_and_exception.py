9541c5f6c66e7f38fa187bf117bfa2d5f60cc0c3
sunjinbo <sunjinbo@outlook.com>
2018-01-19 15:17:37 +0800

    fix build break

--- file_and_exception/file_and_exception.py
+++ file_and_exception/file_and_exception.py
@@ -2,22 +2,25 @@ import os
 
 print(os.getcwd())
 
-data = open('sketch.txt')
+try:
+    data = open('sketch.txt')
 
-print(data.readline())
-print(data.readline())
-print(data.readline())
-print(data.readline())
+    print(data.readline())
+    print(data.readline())
+    print(data.readline())
+    print(data.readline())
 
-data.seek(0)
+    data.seek(0)
 
-for each_line in data:
-    try:
-        if not each_line.find(':') == -1
-            (role,line_spoken) = each_line.split(":")
-            print(role);print(" said: ");print(line_spoken)
-    except:
-        pass
+    for each_line in data:
+        try:
+            if not each_line.find(':') == -1:
+                (role,line_spoken) = each_line.split(":")
+                print(role);print(" said: ");print(line_spoken)
+        except ValueError:
+            pass
 
-data.close()
-
+    data.close()
+except IOError:
+    print('The data file is missing!')
+    
